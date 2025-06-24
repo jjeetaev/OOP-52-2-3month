da5ac55eb4a3ba6a487c85e53b91d9246a5e7b17
Temirlan <kat91971kara@gmail.com>
2025-06-24 18:51:06 +0600

    Second commit: hw4

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main(page: ft.Page):
     page.title = "Мое первое приложение на Flet"    
     page.theme_mode = ft.ThemeMode.LIGHT
 
-    greeting_text = ft.Text("Привет, мир!")
+    greeting_text = ft.Text("Привет, мир!", color="black")
 
     greeting_history = []
 
@@ -20,12 +20,16 @@ def main(page: ft.Page):
 
             if 6 <= current_hour < 12:
                 greeting = "Доброе утро"
+                greeting_text.color = "yellow"
             elif 12 <= current_hour < 18:
                 greeting = "Добрый день"
+                greeting_text.color = "orange"
             elif 18 <= current_hour < 24:
                 greeting = "Добрый вечер"
+                greeting_text.color = "red"
             else:
                 greeting = "Доброй ночи"
+                greeting_text.color = "blue"
 
             greeting_text.value = f"{greeting}, {name}!"
             name_input.value = ""
@@ -35,6 +39,7 @@ def main(page: ft.Page):
             history_text.value = "История приветствий:\n" + "\n".join(greeting_history)
         else:
             greeting_text.value = "Пожалуйста, введите имя."
+            greeting_text.color = "black"
 
         page.update()
 
@@ -45,7 +50,6 @@ def main(page: ft.Page):
             page.theme_mode = ft.ThemeMode.DARK
         else:
             page.theme_mode = ft.ThemeMode.LIGHT
-
         page.update()
 
     def clear_history(_):
@@ -56,22 +60,18 @@ def main(page: ft.Page):
     def copy_greeting(_):
         page.set_clipboard(greeting_text.value)
 
-    clear_button = ft.ElevatedButton("Очистить историю", icon=ft.Icons.DELETE , 
-                                     on_click=clear_history)
+    clear_button = ft.ElevatedButton("Очистить историю", icon=ft.Icons.DELETE, on_click=clear_history)
 
-    theme_button = ft.IconButton(icon=ft.Icons.BRIGHTNESS_6, tooltip='Сменить тему', 
-                                 on_click=toggle_theme)
+    theme_button = ft.IconButton(icon=ft.Icons.BRIGHTNESS_6, tooltip='Сменить тему', on_click=toggle_theme)
 
     greet_button = ft.ElevatedButton("Поздороваться", icon=ft.Icons.HANDSHAKE, on_click=on_button_click)
-    copy_button = ft.IconButton(icon=ft.Icons.COPY, tooltip='Скопировать приветствие', 
-                                on_click=copy_greeting)
-    # page.add(greeting_text, name_input, greet_button, theme_button, clear_button, history_text)
+
+    copy_button = ft.IconButton(icon=ft.Icons.COPY, tooltip='Скопировать приветствие', on_click=copy_greeting)
 
     page.add(
-        # ft.Row([theme_button, clear_button], alignment=ft.MainAxisAlignment.CENTER),
-             ft.Row([greeting_text, copy_button], alignment=ft.MainAxisAlignment.CENTER),
-             ft.Row([name_input, greet_button, theme_button, clear_button], 
-                    alignment=ft.MainAxisAlignment.CENTER),
-             history_text)
+        ft.Row([greeting_text, copy_button], alignment=ft.MainAxisAlignment.CENTER),
+        ft.Row([name_input, greet_button, theme_button, clear_button], alignment=ft.MainAxisAlignment.CENTER),
+        history_text
+    )
 
-ft.app(main, view=ft.WEB_BROWSER)
+ft.app(target=main, view=ft.WEB_BROWSER)
